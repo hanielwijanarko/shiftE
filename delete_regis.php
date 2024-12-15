7db05fb0d109fa9e511a475d39e5733096be1990
YosafatBagusBirawa <yosafat.birawa@mhs.unsoed.ac,id>
2024-12-15 23:02:12 +0700

    fix delete

--- delete_regis.php
+++ delete_regis.php
@@ -1,39 +1,8 @@
 <?php
-require_once 'koneksi.php';
 session_start();
+include("koneksi.php");
 
-$id = $_POST['id'];
-if (isset($_POST['hapus'])) {
-    
-    $sql = "DELETE FROM akun WHERE id = $id";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Data pengguna berhasil dihapus.";
-        header("Location: index_regis.php");
-        exit();
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-} else {
+$id = $_GET['id'];
+    $stmt=mysqli_query($conn, "DELETE FROM akun WHERE id=$id");
+    header("Location: index_regis.php");
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Hapus Akun</title>
-</head>
-<body>
-    <h2>Apakah Anda yakin ingin menghapus akun ini?</h2>
-    <p>Data yang akan dihapus:</p>
-    <ul>
-        </ul>
-    <form method="POST" action="delete_regis.php">
-        <input type="hidden" name="id" value="<?php echo $id; ?>">
-        <input type="submit" name="hapus" value="Hapus">
-        <a href="index_regis.php">Batal</a>
-    </form>
-</body>
-</html>
-
-<?php
-}
